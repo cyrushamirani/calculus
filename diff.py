acf11f781e497b5e460c545151738d3b2b149f48
Cyrus Hamirani <59301843+cyrushamirani@users.noreply.github.com>
2020-01-02 18:37:25 -0800

    Evaluate coefficients and exponents using Function eval
    As opposed to directly converting them to floats, as before.

--- diff.py
+++ diff.py
@@ -7,8 +7,8 @@ from operator import add, sub, mul
 def div(a, b):
     return a / b
 
-#for parsing the function passed in as a string
-operator = {'+': add, '-': sub, '*': mul, '/': div}
+operator = {'+': add, '-': sub, '*': mul, '/': div, 'sin': sin, 'cos': cos}
+constant = {'e': e, 'π': pi}
 
 class Function:
     #Initialize a given function.
@@ -38,10 +38,10 @@ class Function:
                     #Checking to see if there is an exponent. If not, uses default value of 1.
                     try:
                         if term[term.index(var) + 1 : term.index(var) + 2] == '^':
-                            exponent = float(term[term.index(var) + 2:])
+                            exponent = Function(term[term.index(var) + 2:]).evaluate(val, var)
                     finally:  
                         if coefficient:
-                            term = mul(float(coefficient), pow(val, exponent))
+                            term = mul(Function(coefficient).evaluate(val, var), pow(val, exponent))
                         else:
                             term = val
             expression += [term]
@@ -67,3 +67,15 @@ class Function:
             result += self.evaluate(a, var) * step
             a += step
         return round(result, 3)
+        
+new_func = Function('2x^2 + 4x + 10x^3')
+print('Function:', new_func.func)
+
+eval_result = new_func.evaluate(2, 'x')
+print('Eval Result:', eval_result)
+
+diff_result = new_func.differentiate(3, 'x', 0.0001)
+print('Diff Result:', diff_result)
+
+inte_result = new_func.integrate(0, 1, 'x', 0.0001)
+print('Integrate Result:', inte_result)
